83e44d26e4b9c79f9682dbda037af32e0b3a5a2c
Henrique de Paula <henriquedepaula1@hotmail.com>
2023-07-04 14:12:29 -0300

    cadastro de usuario implementado

--- back.php
+++ back.php
@@ -1,65 +1,70 @@
 <?php
 
 //chmando o arquivo que conecta no banco
-require_once('db_login.php');
+require_once('conexao.php');
 
 $loginSucesso = "login efetuado";
 $loginFracasso = "Dados errados";
 
-//verifica se os campos foram preenchidos
-if(isset($_POST['usuario']) && isset($_POST['senha'])){
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
-    //obtem os valores digitados
-    $usuario = addslashes($_POST['usuario']);
-    $senha = addslashes($_POST['senha']);
+    //verifica se o botao de name="login" ou "cadastro" foi clickado
+    if(isset($_POST['login'])){
+        //verifica se os campos foram preenchidos
+        if(isset($_POST['usuario']) && isset($_POST['senha'])){
 
-    //procura os dados no banco
-    $select = "SELECT * FROM login WHERE usuario='" . $usuario . "' AND senha='" . $senha . "'";
-    $stmt = $conn->query($select);
+            //obtem os valores digitados
+            $usuario = addslashes($_POST['usuario']);
+            $senha = addslashes($_POST['senha']);
 
-    // Verifica se os dados foram encontrados
-    $count = $stmt->rowCount();
+            //procura os dados no banco
+            $select = "SELECT * FROM login WHERE usuario='" . $usuario . "' AND senha='" . $senha . "'";
+            $stmt = $conn->query($select);
 
-    //verificando se os dados estao corretos
-    if($count != 0){
-        //alert usando java script
-        echo '<script>alert("' . $loginSucesso . '");</script>';
+            // Verifica se os dados foram encontrados
+            $count = $stmt->rowCount();
+
+            //verificando se os dados estao corretos
+            if($count != 0){
+                //alert usando java script
+                echo '<script>alert("' . $loginSucesso . '");</script>';
+            } else {
+                echo '<script>alert("' . $loginFracasso . '");</script>';
+            }
+        } else {
+            echo '<script>alert("Preencha todos os campos");</script>';
+        } 
+    } elseif (isset($_POST['cadastro'])){
+        //verifica se os campos foram preenchidos
+        if(isset($_POST['usuario']) && isset($_POST['senha'])){
+
+            //obtém os valores digitados
+            $usuario = addslashes($_POST['usuario']);
+            $senha = addslashes($_POST['senha']);
+    
+            //consulta SQL para inserir os dados no banco
+            $insert = "INSERT INTO login (usuario, senha) VALUES ('$usuario', '$senha')";
+    
+            //executa a consulta
+            $stmt = $conn->query($insert);
+    
+            //se $stmt for verdadeiro
+            if($stmt){
+                //registro inserido com sucesso
+                echo '<script>alert("Cadastro realizado com sucesso");</script>';
+            } else {
+                //ocorreu um erro ao inserir o registro
+                echo '<script>alert("Erro ao cadastrar");</script>';
+            }
+        }
     } else {
-        echo '<script>alert("' . $loginFracasso . '");</script>';
+        echo '<script>alert("Preencha todos os campos");</script>';
     }
-} else {
-    echo '<script>alert("Preencha todos os campos");</script>';
+    
 }
-
-
 
 /*-------------------------------------------------------------
 colocar a pasta no C:\xampp\htdocs
 abrir no navegador localhost/nome da pasta/
-*/
-
-
-
-/*-------------------------------------------------------------
-$loginSucesso = "login efetuado";
-$loginFracasso = "Dados errados";
-
-//verifica se os campos foram preenchidos
-if(isset($_POST['usuario']) && isset($_POST['senha'])){
-
-    //obtem os valores digitados
-    $usuario = addslashes($_POST['usuario']);
-    $senha = addslashes($_POST['senha']);
-
-    //verificando se os dados estao corretos
-    if($usuario === "henrique" && $senha === "123"){
-        //alert usando java script
-        echo '<script>alert("' . $loginSucesso . '");</script>';
-    } else {
-        echo '<script>alert("' . $loginFracasso . '");</script>';
-    }
-} else {
-    echo '<script>alert("Preencha todos os campos");</script>';
-}
 */
 ?>
